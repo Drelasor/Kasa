1346ab8f8bf2cb73c645c9fa0b74737e4ca9f3a8
Raphaël Grand <otumbarg@gmail.com>
2024-02-23 15:15:40 +0100

    Création structure

---
+++ src/components/Menu.js
@@ -0,0 +1,10 @@
+export default function Menu(){
+    return(
+        <div className="menu">
+            <ul>
+                <li>Accueil</li>
+                <li>A propos</li>
+            </ul>
+        </div>
+    )
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+export default function Home() {
+return(
+    <div className="home">
+        <h1>chez vous partout et ailleurs</h1>
+    </div>
+)
+}
