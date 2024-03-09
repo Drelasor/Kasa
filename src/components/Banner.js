4bb96d4acec34b7e57c0ab62afe3df4682316687
Raphaël Grand <otumbarg@gmail.com>
2024-03-09 18:45:26 +0100

    premier ajout banner

---
+++ src/components/Banner.js
@@ -0,0 +1,11 @@
+import kasaImg from "../asset/svg/cover.svg"
+
+export default function Banner(){ 
+    return(
+    <div className="text">
+        <img src={kasaImg}></img>
+        <h1>chez vous,partout et ailleurs</h1>
+    </div>
+)   
+    
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,11 @@
 import Card from "../components/Card";
+import Banner from "../components/Banner"
 import data from "../appart.json";
 
 export default function Home() {
 return(
     <div className="home">
-        <div className="text">
-        <img></img>
-        <h1>chez vous partout et ailleurs</h1>
-        </div>
+        <Banner/>
         <div className="infocard">
       {data.map(appart => (
          <Card key={appart.id}  imgUrl={appart.cover} title={appart.title}/>
