f0b825d47ce434086d67601f9d0b4b7e3def53d1
Raphaël Grand <otumbarg@gmail.com>
2024-04-13 23:29:51 +0200

    fin collapse

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -1,5 +1,6 @@
- import {useState } from "react";
+import { useState, useEffect } from "react";
 import vector from "../asset/png/vector.png";
+
 export default function Collapse({ title, content }) {
   const [close, setClose] = useState(true);
 
@@ -7,56 +8,20 @@ export default function Collapse({ title, content }) {
   const handleToggle = () => {
     setClose(!close);
   };
+
   return (
     <div className="collapse">
       <div className="collapse-button" onClick={handleToggle}>
         <h2>{title}</h2>
-        <img src={vector} alt="fleche" className={`arrow ${close ? "close" : ""}`}></img>
+        <img
+          src={vector}
+          alt="fleche"
+          className={`arrow ${close ? "close" : ""}`}
+        ></img>
       </div>
-      <div
-        className={close ? `content animation` : 'content'}
-        // style={{
-        //   maxHeight: `${close ? 0 : CollapseDivOffsetHeight}px`,
-        // }}
-      >
-       {content}
-      </div>
-    </div>
-  );
-}
-
-/* import { useMemo, useRef, useState } from "react";
-import vector from "../asset/png/vector.png";
-export default function Collapse({ title, content }) {
-  const CollapseDiv = useRef(null);
-  const [close, setClose] = useState(true);
-
-  // change etat
-  const handleToggle = () => {
-    setClose(!close);
-  };
-
-  let CollapseDivOffsetHeight = useMemo(() => {
-    return CollapseDiv.current?.scrollHeight ?? 0;
-  }, [CollapseDiv.current]);
-
-  return (
-    <div className= "collapse">
-      <div className="collapse_title" onClick={handleToggle}>
-        <h2>{title}</h2>
-        <img src={vector} alt="fleche" className={`arrow ${close ? "close" : ""}`}></img>
-      </div>
-      <div
-        ref={CollapseDiv}
-        className="content"
-        style={{
-          maxHeight: `${close ? 0 : CollapseDivOffsetHeight}px`,
-        }}
-      >
-        {content}
+      <div className={`content-${close ? "close" : "open"}`}>
+        <p className="text-content">{content}</p>
       </div>
     </div>
   );
 }
- */
-
--- src/pages/Appart.js
+++ src/pages/Appart.js
@@ -1,46 +1,60 @@
-import { useParams } from "react-router-dom";
-import apparts from "../appart.json";
-import Collapse from "../components/Collapse";
-import { Slider } from "../components/Slider";
-import Rating from "../components/Rating";
-import Tag from "../components/Tag";
-import { useNavigate } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom"
+import apparts from "../appart.json"
+import Collapse from "../components/Collapse"
+import { Slider } from "../components/Slider"
+import Rating from "../components/Rating"
+import Tag from "../components/Tag"
+import { useEffect, useState } from "react"
 
 export default function Appart() {
-  const { id } = useParams();
-  const appart = apparts.find((appart) => appart.id === id);
+  const [appart, setAppart] = useState(null)
+  const { id } = useParams()
 
   const navigate = useNavigate()
 
-    if (!appart){
-       return navigate(window.origin + "/error")
-    } 
+  useEffect(() => {
+    const pickedAppart = apparts.find((appart) => appart.id === id)
+
+    if (!pickedAppart) {
+      navigate("/error")
+    } else {
+      setAppart(pickedAppart)
+    }
+  }, [id])
 
   return (
-    <div className="appart">
-      <Slider apparts={appart.pictures} />
-      <div className="appart-title">
-        <h1>{appart.title}</h1>
-        <div className="appart-host">
-          <p>{appart.host.name}</p>
-          <img className="circle" src={appart.host.picture} alt="proprio"></img>
+    <>
+      {appart && (
+        <div className="appart">
+          <Slider apparts={appart.pictures} />
+          <div className="appart-title">
+            <h1>{appart.title}</h1>
+            <div className="appart-host">
+              <p>{appart.host.name}</p>
+              <img
+                className="circle"
+                src={appart.host.picture}
+                alt="proprio"
+              ></img>
+            </div>
+          </div>
+          <p>{appart.location}</p>
+          <div className="tag-rating">
+            <div className="appart-tag">
+              {appart.tags.map((tag, index) => (
+                <Tag key={index} value={tag} />
+              ))}
+            </div>
+            <div className="appart-rating">
+              <Rating rating={appart.rating} />
+            </div>
+          </div>
+          <div className="appart-collapse">
+            <Collapse title={"Description"} content={appart.description} />
+            <Collapse title={"Equipement"} content={appart.equipments} />
+          </div>
         </div>
-      </div>
-      <p>{appart.location}</p>
-      <div className="tag-rating">
-        <div className="appart-tag">
-          {appart.tags.map((tag, index) => (
-            <Tag key={index} value={tag} />
-          ))}
-        </div>
-        <div className="appart-rating">
-          <Rating rating={appart.rating} />
-        </div>
-      </div>
-      <div className="appart-collapse">
-        <Collapse title={"Description"} content={appart.description} />
-        <Collapse title={"Equipement"} content={appart.equipments} />
-      </div>
-    </div>
-  );
+      )}
+    </>
+  )
 }
